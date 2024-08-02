fec1fc10944afa15a0f1f8edfa3ac6aa32ec5284
Manvika Satish <manvikasatish@ip-10-0-0-117.us-west-2.compute.internal>
2024-08-01 17:48:57 -0700

    debugging

--- src/languagetranslation.py
+++ src/languagetranslation.py
@@ -113,7 +113,10 @@ def get_language_code(language):
 
 if __name__ == "__main__":
     language = os.getenv('LANGUAGE')
-    file_path = "../logs/" + os.getenv('FILE_PATH')
+    file_path = "./logs/" + os.getenv('FILE_PATH')
+
+    print(f"Language: {language}")
+    print(f"File Path: {file_path}")
 
     if not file_path:
         raise ValueError("No file path provided in the environment variable 'FILE_PATH'")
